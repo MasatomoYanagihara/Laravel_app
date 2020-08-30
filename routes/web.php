d57ae1d8d441146366f1d3441ae415c9cb355c1e
Masatomo Yanagihara <masatomo.yanagihara.jp@gmail.com>
2020-08-30 20:40:16 +0900

    helloコントローラーを作成しルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get(アドレス, 関数やコントローラー);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); // view()でviews/welcome.blade.phpをレンダリングして表示。
 });
+
+// 第２引数にコントローラーを指定する場合は、'コントローラー名@アクション名'と書く。
+Route::get('hello', 'HelloController@index');
