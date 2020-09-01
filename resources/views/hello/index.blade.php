ce666b6dbbfa0137ba85ff2bdb29f3ac1cd911aa
Masatomo Yanagihara <masatomo.yanagihara.jp@gmail.com>
2020-09-01 14:26:28 +0900

    サブビュー・@eachによるコレクションビュー

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -22,29 +22,30 @@
 
   @extends('layouts.helloapp')
 
-  @section('title', 'Index')
+    @section('title', 'Index')
 
-  @section('menubar')
-  @parent
-    インデックスページ
-  @endsection
+      @section('menubar')
+        @parent
+        インデックスページ
+      @endsection
 
-  @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>必要なだけ記述できます。</p>
-
-  @component('components.message')
-    @slot('msg_title')
-      CAUTION!
-    @endslot
-    @slot('msg_content')
-      これはメッセージの表示です。
-    @endslot
-  @endcomponent
-
-  @endsection
+      @section('content')
+        <p>ここが本文のコンテンツです。</p>
+        <ul>
+          @each('components.item', $data, 'item')
+        </ul>
+      @endsection
 
 
+      @component('components.message')
+        @slot('msg_title')
+          CAUTION!
+        @endslot
+        @slot('msg_content')
+          これはメッセージの表示です。
+        @endslot
+      @endcomponent
+      
   @section('footer')
     copyright 2020 tuyano.
   @endsection
