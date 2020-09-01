157eeef7d5a8d6c5bd08adca8efd256337a49432
Masatomo Yanagihara <masatomo.yanagihara.jp@gmail.com>
2020-09-01 14:14:25 +0900

    コンポーネント使用

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -19,7 +19,7 @@
 </head>
 
 <body>
-  
+
   @extends('layouts.helloapp')
 
   @section('title', 'Index')
@@ -32,7 +32,18 @@
   @section('content')
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
+
+  @component('components.message')
+    @slot('msg_title')
+      CAUTION!
+    @endslot
+    @slot('msg_content')
+      これはメッセージの表示です。
+    @endslot
+  @endcomponent
+
   @endsection
+
 
   @section('footer')
     copyright 2020 tuyano.
