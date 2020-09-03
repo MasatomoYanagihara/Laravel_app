d1e9a582bc8fbd32a107258d69b27a1479d95113
Yana <masatomo.yanagihara.jp@gmail.com>
2020-09-03 14:16:42 +0900

    DB::deleteでレコード削除

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,12 @@ Route::get('/', function () {
 // 第２引数にコントローラーを指定する場合は、'コントローラー名@アクション名'と書く。
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
+
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
+
 Route::get('hello/edit', 'HelloController@edit');
 Route::post('hello/edit', 'HelloController@update');
+
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
