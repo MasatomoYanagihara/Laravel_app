370ccf710e7bb7c53a3fc139bc60ce82a37ad0cd
Masatomo Yanagihara <masatomo.yanagihara.jp@gmail.com>
2020-09-04 16:36:34 +0900

    ペジネーション

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,70 +1,54 @@
-<html>
+@extends('layouts.helloapp')
+<style>
+  .pagination {
+    font-size: 10pt;
+  }
 
-<head>
-  <title>Hello/Index</title>
-  <style>
-    body {
-      font-size: 16pt;
-      color: #999;
-    }
+  .pagination li {
+    display: inline-block
+  }
 
-    h1 {
-      font-size: 50pt;
-      text-align: right;
-      color: #f6f6f6;
-      margin: -20px 0px -30px 0px;
-      letter-spacing: -4pt;
-    }
-  </style>
-</head>
+  tr th a:link {
+    color: white;
+  }
 
-<body>
+  tr th a:visited {
+    color: white;
+  }
 
-  @extends('layouts.helloapp')
+  tr th a:hover {
+    color: white;
+  }
 
-  @section('title', 'Index')
+  tr th a:active {
+    color: white;
+  }
+</style>
+@section('title', 'Index')
 
-  @section('menubar')
-  @parent
-  インデックスページ
-  @endsection
+@section('menubar')
+@parent
+インデックスページ
+@endsection
 
+@section('content')
+<table>
+  <tr>
+    <th><a href="/hello?sort=name">name</a></th>
+    <th><a href="/hello?sort=mail">mail</a></th>
+    <th><a href="/hello?sort=age">age</a></th>
+  </tr>
+  @foreach ($items as $item)
+  <tr>
+    <td>{{$item->name}}</td>
+    <td>{{$item->mail}}</td>
+    <td>{{$item->age}}</td>
+  </tr>
+  @endforeach
+</table>
+{{ $items->appends(['sort' => $sort])->links() }}
+@endsection
 
-
-
-
-  @section('content')
-  <table>
-    <tr>
-      <th>Name</th>
-      <th>Mail</th>
-      <th>Age</th>
-    </tr>
-    @foreach ($items as $item)
-    <tr>
-      <td>{{$item->name}}</td>
-      <td>{{$item->mail}}</td>
-      <td>{{$item->age}}</td>
-    </tr>
-    @endforeach
-  </table>
-  @endsection
-
-
-
-  @component('components.message')
-  @slot('msg_title')
-  CAUTION!
-  @endslot
-  @slot('msg_content')
-  これはメッセージの表示です。
-  @endslot
-  @endcomponent
-
-  @section('footer')
-  copyright 2020 tuyano.
-  @endsection
-</body>
-
-
-</html>
+@section('footer')
+copyright 2020 tuyano.
+@endsection
